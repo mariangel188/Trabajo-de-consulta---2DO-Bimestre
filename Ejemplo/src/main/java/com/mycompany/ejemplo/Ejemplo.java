feea9c1f572f4c0788a069e89f1e7639cace862d
Mariangel Rojas <marojas31@utpl.edu.ec>
2024-02-03 18:17:14 -0500

    Ejercicio completo

---
+++ Ejemplo/src/main/java/com/mycompany/ejemplo/Ejemplo.java
@@ -0,0 +1,37 @@
+
+package com.mycompany.ejemplo;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
+public class Ejemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Proceso iniciado");
+        Observable<String> frutasObservable = Observable.just("Fresa", "Manzana", "Uva", "Mandarina", "Banana");
+        frutasObservable
+                .map(fruta -> fruta.toUpperCase()) 
+                .filter(fruta -> fruta.startsWith("M")) 
+                .subscribe(new Observer<String>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
+
+                    @Override
+                    public void onNext(String fruta) {
+                        System.out.println("Fruta obtenida: " + fruta);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        System.out.println("Proceso listo y completo");
+                    }
+                });
+    }
+}
